d8cc3b4d62162c1d98e2a290dae1e82b7170adfd
Taha Bohra <tahabohra@Tahas-MacBook-Air.local>
2026-05-10 01:57:51 +0530

    import/export support added

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
 from scraper.models import RawTrialData, Trial, TrialChangeSummary, TrialVersion
 
 # Register your models here.
 
+@admin.register(RawTrialData)
+class RawTrialDataAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(TrialVersion)
+class TrialVersionAdmin(ImportExportModelAdmin):
+    pass
+
+# @admin.register(RawTrialData)
+# class RawTrialDataAdmin(ImportExportModelAdmin):
+#     pass
+
 admin.site.register(Trial)
-admin.site.register(RawTrialData)
-admin.site.register(TrialVersion)
 admin.site.register(TrialChangeSummary)
